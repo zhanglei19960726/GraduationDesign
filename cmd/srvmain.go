026723631a0b1f673de58291c2f4fe908fe9cf78
zhanglei19960726 <825309902@qq.com>
2018-05-06 14:25:53 +0800

    fist commit

---
+++ cmd/srvmain.go
@@ -0,0 +1,9 @@
+
+package main
+
+import "GraduationDesign/srv"
+
+func main() {
+        srv.Run()
+
+}
---
+++ srv/srv.go
@@ -0,0 +1,59 @@
+package srv
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"sort"
+	"strings"
+)
+
+const (
+	token = "zhang"
+)
+
+func makeSignature(timestamp, nonce string) string {
+	sl := []string{token, timestamp, nonce}
+	sort.Strings(sl)
+	s := sha1.New()
+	io.WriteString(s, strings.Join(sl, ""))
+	return fmt.Sprintf("%x", s.Sum(nil))
+
+}
+
+func validateUrl(w http.ResponseWriter, r *http.Request) bool {
+	timestamp := strings.Join(r.Form["timestamp"], "")
+	nonce := strings.Join(r.Form["nonce"], "")
+	signatureGen := makeSignature(timestamp, nonce)
+	signatureIn := strings.Join(r.Form["signature"], "")
+	echostr := strings.Join(r.Form["echostr"], "")
+	if signatureGen != signatureIn {
+		return false
+	}
+	fmt.Fprintf(w, echostr)
+	return true
+
+}
+func procRequest(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	if !validateUrl(w, r) {
+		log.Println("Wechat Service: this http request is not from Wechat platform!")
+		return
+
+	}
+	log.Println("Wechat Service: validateUrl Ok!")
+}
+
+func Run() {
+	log.Println("Wechat Service: Start!")
+	http.HandleFunc("/", procRequest)
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal("Wechat Service: ListenAndServe failed, ", err)
+
+	}
+	log.Println("Wechat Service: Stop!")
+
+}
