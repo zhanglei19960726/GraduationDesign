6c63791da790634eb1895a7862bda66582300bfa
心碎了无痕 <825309902@qq.com>
2018-05-10 05:23:34 +0800

    1

--- client/diyMen/men.go
+++ client/diyMen/men.go
@@ -35,21 +35,35 @@ func pushWxMenuCreate(accessToken string, menuJsonBytes []byte) error {
 }
 
 func CreateWxMenu() error {
-	menuStr := ` {
-    "button": [ 
-        	{
-            	"name": "菜单", 
-            	"sub_button": [
-                	{
-                    	"type": "view", 
-                    	"name": "课件", 
-                    	"key": "V1",
-						"url":"ftp://140.143.14.180/kejian/",
-						"sub_button": [ ]
-                	}
-            	]
-        	},
-	}`
+	menuStr := `{
+     "button":[
+     {    
+          "type":"click",
+          "name":"今日歌曲",
+          "key":"V1001_TODAY_MUSIC"
+      },
+      {
+           "name":"菜单",
+           "sub_button":[
+           {    
+               "type":"view",
+               "name":"搜索",
+               "url":"http://www.soso.com/"
+            },
+            {
+                 "type":"miniprogram",
+                 "name":"wxa",
+                 "url":"http://mp.weixin.qq.com",
+                 "appid":"wx286b93c14bbf93aa",
+                 "pagepath":"pages/lunar/index"
+             },
+            {
+               "type":"click",
+               "name":"赞一下我们",
+               "key":"V1001_GOOD"
+            }]
+       }]
+ }`
 	err := WxPlatUtil.GetAndUpdateDBWxAToken()
 	if err != nil {
 		log.Println(err.Error())
