5266c310910e31d9c631ddf43404622fb8706828
心碎了无痕 <825309902@qq.com>
2018-05-14 15:31:54 +0800

    1

--- wxclient/srv.go
+++ wxclient/srv.go
@@ -1,17 +1,19 @@
 package wxclient
 
 import (
+	"io/ioutil"
 	"net/http"
 	"os"
 )
 
 func readHTML() ([]byte, error) {
 	var buf []byte
-	file, err := os.Open("../html/admin.html")
+	goPath := os.Getenv("GOPATH")
+	file, err := os.Open(goPath + "\\src\\GraduationDesign\\html\\admin.html")
 	if err != nil {
 		return buf, err
 	}
-	_, err = file.Read(buf)
+	buf, err = ioutil.ReadAll(file)
 	if err != nil {
 		return buf, err
 	}
@@ -19,5 +21,17 @@ func readHTML() ([]byte, error) {
 }
 
 func AdminHanler(w http.ResponseWriter, r *http.Request) {
-
+	buf, err := readHTML()
+	if err != nil {
+		panic(err.Error())
+		return
+	}
+	w.Write(buf)
+	if r.Method == "POST" {
+		title := r.FormValue("title")
+		author := r.FormValue("author")
+		digest := r.FormValue("digest")
+		content := r.FormValue("content")
+		w.Write([]byte("title is :" + title + " autor is :" + author + " digest is :" + digest + " content:" + content))
+	}
 }
