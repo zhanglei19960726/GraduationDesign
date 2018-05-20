f2756f2ea526bd8298a00ad498ea5ab7d7ea391d
心碎了无痕 <825309902@qq.com>
2018-05-20 21:26:11 +0800

    1

--- wxsrv/weixinsrv.go
+++ wxsrv/weixinsrv.go
@@ -68,26 +68,25 @@ func createMenu(wx *weixin.Weixin) error {
 
 //接收点击菜单跳转链接时的事件
 func eventView(writer weixin.ResponseWriter, request *weixin.Request) {
-	if request.EventKey == databaseIntroductionKey {
-		article := make([]weixin.Article, 1)
-		article[0].Title = "数据库简介"
-		article[0].Description = "数据库(Database)是按照数据结构来组织、存储和管理数据的仓库，" +
-			"它产生于距今六十多年前，随着信息技术和市场的发展，特别是二十世纪九十年代以后，" +
-			"数据管理不再仅仅是存储和管理数据，而转变成用户所需要的各种数据管理的方式。数据库有很多种类型，" +
-			"从最简单的存储有各种数据的表格到能够进行海量数据存储的大型数据库系统都在各个方面得到了广泛的应用。" +
-			"在信息化社会，充分有效地管理和利用各类信息资源，是进行科学研究和决策管理的前提条件。" +
-			"数据库技术是管理信息系统、办公自动化系统、决策支持系统等各类信息系统的核心部分，" +
-			"是进行科学研究和决策管理的重要技术手段"
-		err := writer.PostNews(article)
-		if err != nil {
-			log.Println(err.Error())
-			return
+	if request.Event == weixin.MenuButtonTypeKey {
+		if request.EventKey == databaseIntroductionKey {
+			article := make([]weixin.Article, 1)
+			article[0].Title = "数据库简介"
+			article[0].Description = "数据库(Database)是按照数据结构来组织、存储和管理数据的仓库，" +
+				"它产生于距今六十多年前，随着信息技术和市场的发展，特别是二十世纪九十年代以后，" +
+				"数据管理不再仅仅是存储和管理数据，而转变成用户所需要的各种数据管理的方式。数据库有很多种类型，" +
+				"从最简单的存储有各种数据的表格到能够进行海量数据存储的大型数据库系统都在各个方面得到了广泛的应用。" +
+				"在信息化社会，充分有效地管理和利用各类信息资源，是进行科学研究和决策管理的前提条件。" +
+				"数据库技术是管理信息系统、办公自动化系统、决策支持系统等各类信息系统的核心部分，" +
+				"是进行科学研究和决策管理的重要技术手段"
+			err := writer.PostNews(article)
+			if err != nil {
+				log.Println(err.Error())
+				return
+			}
+		} else if request.EventKey == talkSpace {
+			writer.ReplyText("haha")
 		}
-	}
-	if request.EventKey == talkSpace {
-		fmt.Println("hahahahaah")
-		wx := writer.GetWeixin()
-		wx.CreateRedirectURL("http://www.baid.com", weixin.RedirectURLScopeBasic, "")
 	}
 }
 
