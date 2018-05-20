ebe799be47bdcc5823533c483835e53445a93015
心碎了无痕 <825309902@qq.com>
2018-05-20 23:21:29 +0800

    1

--- wxsrv/weixinsrv.go
+++ wxsrv/weixinsrv.go
@@ -32,7 +32,10 @@ func echo(w weixin.ResponseWriter, r *weixin.Request) {
 		log.Println("robort error :", err.Error())
 		return
 	}
-	w.ReplyText(res.Results[0].Values.Text)
+	for _, v := range res.Results {
+		w.ReplyText(v.Values.Text)
+	}
+
 }
 
 //关注事件的处理函数
