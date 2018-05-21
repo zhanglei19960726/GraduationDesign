8ab4d00bb452a58b254f565b56a270cbf359eda5
心碎了无痕 <825309902@qq.com>
2018-05-21 16:16:34 +0800

    1

--- wxsrv/weixinsrv.go
+++ wxsrv/weixinsrv.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	url2 "net/url"
 	"os"
 )
 
@@ -209,7 +210,15 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func userAgree() {
-	request, err := http.NewRequest("GET", "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base&state=123#wechat_redirect", nil)
+	wxUrl := "https://open.weixin.qq.com/connect/oauth2/authorize" +
+		"?appid=" + appID +
+		"&redirect_uri=" + redirectUri +
+		"&response_type=code&scope=snsapi_base&state=123#wechat_redirect"
+	parse, err := url2.ParseQuery(wxUrl)
+	if err != nil {
+		log.Println("parse url error:", err.Error())
+	}
+	request, err := http.NewRequest("GET", parse.Encode(), nil)
 	if err != nil {
 		log.Println("new request error:", err.Error())
 		return
