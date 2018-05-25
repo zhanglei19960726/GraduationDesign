b24b94c1cfaf9401fd7933b5156d637dc1c962e5
心碎了无痕 <825309902@qq.com>
2018-05-25 11:50:49 +0800

    1

--- wxsrv/weixinsrv.go
+++ wxsrv/weixinsrv.go
@@ -1,8 +1,8 @@
 package wxsrv
 
 import (
-	"bytes"
-	"encoding/json"
+	"GraduationDesign/msgtype"
+	"GraduationDesign/wxclient"
 	"fmt"
 	"github.com/wizjin/weixin"
 	"io/ioutil"
@@ -17,25 +17,18 @@ var (
 )
 
 const (
-	token                   = "zhang"
-	appID                   = "wxf4b1e3a9d5753984"
-	appSecret               = "c8981b2fc40b3ecc24f22dc644829099"
-	databaseIntroductionKey = "Mykey001"
-	talkSpace               = "Mykey002"
-	redirectUri             = "http://www.zhangleispace.club/upload"
+	token          = "zhang"
+	appID          = "wxf4b1e3a9d5753984"
+	appSecret      = "c8981b2fc40b3ecc24f22dc644829099"
+	studyOnlineKey = "Mykey001"
+	talkSpace      = "Mykey002"
+	sqlKey         = "Mykey003"
+	sqlModlekey    = "Mykey004"
+	redirectUri    = "http://www.zhangleispace.club/upload"
 )
 
 //文本消息的处理函数
 func echo(w weixin.ResponseWriter, r *weixin.Request) {
-	text := r.Content
-	res, err := robort(text)
-	if err != nil {
-		log.Println("robort error :", err.Error())
-		return
-	}
-	for _, v := range res.Results {
-		w.ReplyText(v.Values.Text)
-	}
 
 }
 
@@ -49,10 +42,15 @@ func subscribe(writer weixin.ResponseWriter, request *weixin.Request) {
 //创建菜单
 func createMenu(wx *weixin.Weixin) error {
 	menu := &weixin.Menu{make([]weixin.MenuButton, 3)}
-	menu.Buttons[0].Name = "数据库简介"
-	menu.Buttons[0].Type = weixin.MenuButtonTypeKey
-	menu.Buttons[0].Key = databaseIntroductionKey
-	menu.Buttons[1].Name = "数据库教程"
+	menu.Buttons[0].Name = "在线学习"
+	menu.Buttons[0].SubButtons = make([]weixin.MenuButton, 2)
+	menu.Buttons[0].SubButtons[0].Name = "sql 语句"
+	menu.Buttons[0].SubButtons[0].Key = sqlKey
+	menu.Buttons[0].SubButtons[0].Type = weixin.MenuButtonTypeKey
+	menu.Buttons[0].SubButtons[1].Name = "数据库模式"
+	menu.Buttons[0].SubButtons[1].Key = sqlModlekey
+	menu.Buttons[0].SubButtons[1].Type = weixin.MenuButtonTypeKey
+	menu.Buttons[1].Name = "精彩案例"
 	menu.Buttons[1].SubButtons = make([]weixin.MenuButton, 2)
 	menu.Buttons[1].SubButtons[0].Name = "mysql教程"
 	menu.Buttons[1].SubButtons[0].Type = weixin.MenuButtonTypeUrl
@@ -60,7 +58,7 @@ func createMenu(wx *weixin.Weixin) error {
 	menu.Buttons[1].SubButtons[1].Name = "sql server 教程"
 	menu.Buttons[1].SubButtons[1].Type = weixin.MenuButtonTypeUrl
 	menu.Buttons[1].SubButtons[1].Url = "http://www.runoob.com/sql/sql-tutorial.html"
-	menu.Buttons[2].Name = "讨论区"
+	menu.Buttons[2].Name = "在线指导"
 	menu.Buttons[2].Type = weixin.MenuButtonTypeUrl
 	menu.Buttons[2].Url = "http://www.runoob.com/sql/sql-tutorial.html"
 	err := wx.CreateMenu(menu)
@@ -72,28 +70,24 @@ func createMenu(wx *weixin.Weixin) error {
 
 //接收点击菜单跳转链接时的事件
 func eventView(writer weixin.ResponseWriter, request *weixin.Request) {
-	if request.EventKey == databaseIntroductionKey {
-		wx := writer.GetWeixin()
-		url := wx.CreateRedirectURL(redirectUri, weixin.RedirectURLScopeUserInfo, "")
-		fmt.Println(url)
-		//userAgree(url)
-		article := make([]weixin.Article, 1)
+	if request.EventKey == sqlKey {
+		mediaId, err := writer.UploadMediaFromFile(weixin.MediaTypeImage, filePath+"1.png")
+		if err != nil {
+			fmt.Println("upload pircture err:", err.Error())
+			return
+		}
+		article := make([]msgtypetype.Articles, 1)
 		article[0].Title = "数据库简介"
-		article[0].Description = "数据库(Database)是按照数据结构来组织、存储和管理数据的仓库，" +
-			"它产生于距今六十多年前，随着信息技术和市场的发展，特别是二十世纪九十年代以后，" +
-			"数据管理不再仅仅是存储和管理数据，而转变成用户所需要的各种数据管理的方式。数据库有很多种类型，" +
-			"从最简单的存储有各种数据的表格到能够进行海量数据存储的大型数据库系统都在各个方面得到了广泛的应用。" +
-			"在信息化社会，充分有效地管理和利用各类信息资源，是进行科学研究和决策管理的前提条件。" +
-			"数据库技术是管理信息系统、办公自动化系统、决策支持系统等各类信息系统的核心部分，" +
-			"是进行科学研究和决策管理的重要技术手段"
-		article[0].Url = "http://www.zhangleispace.club/upload"
-		err := writer.PostNews(article)
+		article[0].Content = "zhanglei111111111111111112344455"
+		article[0].ShowCoverPic = 1
+		article[0].ThumbMediaId = mediaId
+		_, err = wxclient.AddNews(article)
 		if err != nil {
 			log.Println(err.Error())
 			return
 		}
 	} else if request.EventKey == talkSpace {
-		writer.ReplyText("haha")
+
 	}
 }
 
@@ -116,106 +110,6 @@ func userAgree(url string) {
 		return
 	}
 	fmt.Println(string(body))
-}
-
-type Text struct {
-	Text string `json:"text"`
-}
-
-type Percption struct {
-	InputText Text `json:"inputText"`
-}
-
-type UserInfo struct {
-	ApiKey string `json:"apiKey"`
-	UserId string `json:"userId"`
-}
-
-type Robort struct {
-	ReqType    int       `json:"reqType"`
-	Perception Percption `json:"perception"`
-	UserInfo   UserInfo  `json:"userInfo"`
-}
-
-const (
-	apiKey = "ed22295b09924bbab90573f18a4449bc"
-	userId = appID
-)
-
-type Response struct {
-	Emotion Emotion   `json:"emotion"`
-	Intent  Intent    `json:"intent"`
-	Results []Results `json:"results"`
-}
-
-type Emotion struct {
-	RobotEmotion RobotEmotion `json:"robotEmotion"`
-	UserEmotion  UserEmotion  `json:"userEmotion"`
-}
-
-type UserEmotion struct {
-	A         int `json:"a"`
-	D         int `json:"d"`
-	EmotionId int `json:"emotionId"`
-	P         int `json:"p"`
-}
-
-type RobotEmotion struct {
-	A         int `json:"a"`
-	D         int `json:"d"`
-	EmotionId int `json:"emotionId"`
-	P         int `json:"p"`
-}
-
-type Intent struct {
-	Code       int    `json:"code"`
-	ActionName string `json:"actionName"`
-	IntentName string `json:"intentName"`
-}
-
-type Results struct {
-	ResultType string `json:"resultType"`
-	Values     Text   `json:"values"`
-	GroupType  int    `json:"groupType"`
-}
-
-func robort(text string) (res *Response, err error) {
-	robort := &Robort{
-		Perception: Percption{
-			InputText: Text{
-				Text: text,
-			},
-		},
-		UserInfo: UserInfo{
-			ApiKey: apiKey,
-			UserId: userId,
-		},
-	}
-	data, err := json.Marshal(robort)
-	if err != nil {
-		log.Println("json error:", err.Error())
-		return
-	}
-	request, err := http.NewRequest("POST", "http://openapi.tuling123.com/openapi/api/v2", bytes.NewBuffer(data))
-	if err != nil {
-		log.Println("make request error:", err.Error())
-		return
-	}
-	client := &http.Client{}
-	resp, err := client.Do(request)
-	if err != nil {
-		log.Println("send message error:", err.Error())
-		return
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		log.Println("get response error :", err.Error())
-		return
-	}
-	res = &Response{}
-	err = json.Unmarshal(body, res)
-	return
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
