9d0e9c3d901346821024afeaa95b53baf600e3ac
心碎了无痕 <825309902@qq.com>
2018-05-26 15:04:30 +0800

    1

--- wxsrv/weixinsrv.go
+++ wxsrv/weixinsrv.go
@@ -41,6 +41,7 @@ func sendOneArticle(w weixin.ResponseWriter, title, picUrl, articleurl, descript
 //文本消息的处理函数
 func echo(w weixin.ResponseWriter, r *weixin.Request) {
 	media, err := w.UploadMediaFromFile(weixin.MediaTypeImage, path+"sql.jpg")
+	fmt.Println(media)
 	if err != nil {
 		log.Println(err.Error())
 		return
